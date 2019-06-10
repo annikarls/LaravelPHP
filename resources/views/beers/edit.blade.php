0001ce49ba0d06f3029550ed17e3899d97e439d2
Annika <karlsson.e.annika@gmail.com>
2019-06-10 14:51:52 +0200

    update

--- resources/views/beers/edit.blade.php
+++ resources/views/beers/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('header')
 @section('title', 'Öl')
 @section('content')
-    <h1 class="title">Redigera/ta bort en öl</h1>
+    <h1 class="title">Redigera</h1>
     <form method="POST" action="/beers/{{ $beer->beerId }}">
         {{ method_field('PATCH') }}
         {{ csrf_field() }}
