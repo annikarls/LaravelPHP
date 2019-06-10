96f5b259cf96cdede505572177c99304622deced
Elin Gustafsson <elin.b.gustafsson@gmail.com>
2019-06-10 22:32:34 +0200

    update

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -6,18 +6,18 @@
   <a class="button is-link" href="/events/create">Lägg till nytt event</a>
         @foreach ($events as $event)
 
+        <div class="column eventColumn is-5">
+            <h1 class="title">{{ $event->eventTitle }}</h1>
+            <div class="img__wrap">
+              <img class="img__img" src="{{ $event->eventImage }}">
+            </div>
             <p>{{ $event->eventTitle }}</p>
             <p>{{ $event->eventDescription }}</p>
             <p>{{ $event->eventLocation }}</p>
             <p>{{ $event->eventDate }}</p>
             <p>{{ $event->eventTime }}</p>
-            <a class="button is-success is-outlined" href="/events/{{ $event->eventId }}/edit">Redigera</a>    
-    </ul>
-      
-
-        <div class="column eventColumn is-5">
-            <h1 class="title">{{ $event->eventTitle }}</p>
-            <a class="button is-success" href="/events/{{ $event->eventId }}/edit">Redigera</a>
+            <a class="button is-success is-outlined" href="/events/{{ $event->eventId }}/edit">Redigera</a>  
+            <!-- <a class="button is-success" href="/events/{{ $event->eventId }}/edit">Redigera</a> -->
 
     <form action="/events/{{ $event->eventId }}" method="POST">
         @method('DELETE')
@@ -26,9 +26,6 @@
             <div class="control">
                 <button class="button is-danger" type="submit">Ta bort</button>
             </div>
-        </div>
-        <div class="img__wrap">
-          <img class="img__img" src="{{ $event->eventImage }}">
         </div>
     </form>
     </div>
