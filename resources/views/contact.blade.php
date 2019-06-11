7112afb217da1a2d73c6d6dbd2e1108f32816d88
Yasmine Aikio <yyaasse@hotmail.com>
2019-06-11 12:56:05 +0200

    logo

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Kontakt')
 @section('content')
       <div class="column is-4 contactUsText">
+        <h1 class="title logo">ö.l</h1>
         <h1 class="title">Om oss</h1>
         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
       </div>
