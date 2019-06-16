a059ea6ab905e108315d124a626cb96db944a151
Yasmine Aikio <yyaasse@hotmail.com>
2019-06-16 22:42:56 +0200

    css frontpage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,79 @@
 swal("Är du 18 år eller äldre?", "För att läsa den här sidans innehåll bör du vara 18 år eller äldre.","warning");
 </script> -->
   <div class="column welcome">
-    <p>Kolumn 1</p>
+    <div class="welcomeText">
+      <h1 class="title welcomeTitle">Välkommen!</h1>
+      <p>På den här sidan hittar du noga utvalda öl-sorter. Lokala, för alla och UTSÖKTA.</p>
+    </div>
+
+    <?php $beer = DB::table('beers')->where('beerType', 'porter')->first(); ?>
+      <div class="img__wrapw">
+          <img class="img__imgw" src="{{ $beer->beerImage }}">
+              <div class="img__description_layerw">
+                  <p class="img__descriptionw">{{ $beer->beerType }}</p>
+              </div>
+      </div>
+      <?php $beer = DB::table('beers')->where('beerType', 'nonalco')->first(); ?>
+        <div class="img__wrapw">
+            <img class="img__imgw" src="{{ $beer->beerImage }}">
+                <div class="img__description_layerw">
+                    <p class="img__descriptionw">{{ $beer->beerType }}</p>
+                </div>
+        </div>
+        <?php $beer = DB::table('beers')->where('beerType', 'ipa')->first(); ?>
+          <div class="img__wrapw">
+              <img class="img__imgw" src="{{ $beer->beerImage }}">
+                  <div class="img__description_layerw">
+                      <p class="img__descriptionw">{{ $beer->beerType }}</p>
+                  </div>
+          </div>
   </div>
   <div class="column welcome2">
-    <p>Kolumn 2</p>
+    <?php $beer = DB::table('beers')->where('beerType', 'suröl')->first(); ?>
+      <div class="img__wrapw">
+          <img class="img__imgw" src="{{ $beer->beerImage }}">
+              <div class="img__description_layerw">
+                  <p class="img__descriptionw">{{ $beer->beerType }}</p>
+              </div>
+      </div>
+      <?php $beer = DB::table('beers')->where('beerType', 'lager')->first(); ?>
+        <div class="img__wrapw">
+            <img class="img__imgw" src="{{ $beer->beerImage }}">
+                <div class="img__description_layerw">
+                    <p class="img__descriptionw">{{ $beer->beerType }}</p>
+                </div>
+        </div><?php $beer = DB::table('beers')->where('beerType', 'stout')->first(); ?>
+          <div class="img__wrapw">
+              <img class="img__imgw" src="{{ $beer->beerImage }}">
+                  <div class="img__description_layerw">
+                      <p class="img__descriptionw">{{ $beer->beerType }}</p>
+                  </div>
+          </div>
   </div>
   <div class="column welcome3">
-    <p>Kolumn 3</p>
+    <?php $beer = DB::table('beers')->where('beerType', 'apa')->first(); ?>
+      <div class="img__wrapw">
+          <img class="img__imgw" src="{{ $beer->beerImage }}">
+              <div class="img__description_layerw">
+                  <p class="img__descriptionw">{{ $beer->beerType }}</p>
+              </div>
+      </div>
+      <?php $beer = DB::table('beers')->where('beerType', 'folköl')->first(); ?>
+        <div class="img__wrapw">
+            <img class="img__imgw" src="{{ $beer->beerImage }}">
+                <div class="img__description_layerw">
+                    <p class="img__descriptionw">{{ $beer->beerType }}</p>
+                </div>
+        </div>
+        <?php $beer = DB::table('beers')->where('beerType', 'veteöl')->first(); ?>
+          <div class="img__wrapw">
+              <img class="img__imgw" src="{{ $beer->beerImage }}">
+                  <div class="img__description_layerw">
+                      <p class="img__descriptionw">{{ $beer->beerType }}</p>
+                  </div>
+          </div>
   </div>
   </div>
 @endsection
 </body>
-</html>
+</html
