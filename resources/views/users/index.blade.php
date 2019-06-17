1edf1b80c272b03b6f970395e07e3a54bacf9c01
Annika <karlsson.e.annika@gmail.com>
2019-06-17 15:06:19 +0200

    update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,17 +3,24 @@
 @section('content')
 @if (Auth::user())
 <div class="column regForm">
-    <p class="is-size-5">Användarnamn: {{ $users->name }}</p>
+    <h1 class="title">Välkommen {{ $users->name }}!</h1>
     <p class="is-size-5">Email: {{ $users->email }}</p>
     <br>
-    <h1 class="title">Öl tillagd av {{ $users->name }}:</h1>
-    <ul class="content">
+    <table class="table">
+        <tr>
+            <th>Öl tillagd av {{ $users->name }}</th>
+            <th></th>
+            <th></th>
+        </tr>
         @foreach ($users->beers as $beer)
-            <li>{{ $beer->beerName }} <a class="button is-small" href="/beers/{{ $beer->id }}/edit">Redigera</a>
-                <button class="button is-danger is-small" type="submit">Ta bort</button></li>
+        <tr>
+            <td>{{ $beer->beerName }}</td>
+            <td><a class="button is-small" href="/beers/{{ $beer->id }}/edit">Redigera</a></td>
+            <td><button class="button is-danger is-small" type="submit">Ta bort</button></td>
+        </tr>
         @endforeach
-    </ul>
-    <a class="button is-link" href="/beers/create">Lägg till ny öl</a>
+    </table>
+    <a class="button" href="/beers/create">Lägg till ny öl</a>
 </div>
 @endif
 
