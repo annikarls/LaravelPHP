6e7d5189f6a1066b537ea520e002c0051913566b
Annika <karlsson.e.annika@gmail.com>
2019-06-17 13:46:07 +0200

    update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,9 @@ use Illuminate\Support\Facades\Auth;
 class UsersController extends Controller
 {
     public function index() {
+        $userId = Auth::id();
         $users = User::all();
+        $users = $users->find($userId);
         return view('users.index', compact('users'));
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,10 +3,13 @@
 @section('content')
 
 <div class="column regForm">
-    <p class="is-size-4">Användarnamn: {{ Auth::user()->name }}</p>
-    <p class="is-size-4">Email: {{ Auth::user()->email }}</p>
-    <p><?php $beers = App\Beer::whereUserId(Auth::id())->get(); ?></p>
-<p>{{ $beers }}</p>
+    <p class="is-size-4">Användarnamn: {{ $users->name }}</p>
+    <p class="is-size-4">Email: {{ $users->email }}</p>
+    <ul class="content">
+        @foreach ($users->beers as $beer)
+            <li>{{ $beer->beerName }}</li>
+        @endforeach
+    </ul>
 </div>
 @endsection
 </body>
