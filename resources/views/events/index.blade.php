2d17ffa3905446c474b73e05a287091c0b17a1b1
Yasmine Aikio <yyaasse@hotmail.com>
2019-06-18 09:48:02 +0200

    fixed button event inloggad

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -7,7 +7,7 @@
 <div class="column eventup is-10">
   <h1 class="title ourevents">Våra event</h1>
   <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-</div>
+  <br>
   <div class="field">
       <div class="control">
               @if (Auth::user())
@@ -15,19 +15,10 @@
           @endif
       </div>
   </div>
+</div>
+
         @foreach ($events as $event)
         <div class="column eventColumn is-6">
-            <!-- <h1 class="title">{{ $event->eventTitle }}</h1> -->
-            <!-- <div class="img__wrap">
-              <img class="img__img" src="{{ $event->eventImage }}">
-            </div> -->
-            <!-- <p>{{ $event->eventTitle }}</p>
-            <p>{{ $event->eventDescription }}</p>
-            <p>{{ $event->eventLocation }}</p>
-            <p>{{ $event->eventDate }}</p>
-            <p>{{ $event->eventTime }}</p>
-            <a class="button is-success is-outlined" href="/events/{{ $event->eventId }}/edit">Redigera</a> -->
-            <!-- <a class="button is-success" href="/events/{{ $event->eventId }}/edit">Redigera</a> -->
 
             <form action="/events/{{ $event->eventId }}" method="POST">
               @method('DELETE')
